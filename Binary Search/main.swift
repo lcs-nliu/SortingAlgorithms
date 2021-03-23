9002980e04f7eecbc9ad95cf37a260553a42d3aa
Nicole Liu <nicoleliu_@outlook.com>
2021-03-23 08:57:40 -0400

    Made a bubble sort algorithm

---
+++ Binary Search/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Binary Search
+//
+//  Created by Liu, Bojun on 2021-03-23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- SortingAlgorithms/main.swift
+++ SortingAlgorithms/main.swift
@@ -28,31 +28,45 @@ waitForInput()
 
 // Sort the array
 
+var sorted = 0
+var isSwapping = false
+
 // Loop through the entire array 'n' times
 // However many times there are elements in the array
 for i in 0..<dataSet.count {
     
+    isSwapping = false
     
     // One pass through the array to float the highest number to the end
-    for j in 0..<dataSet.count - 1 {
+    for j in 0..<dataSet.count - sorted - 1 {
         
         // Compare left value to right value
-        
         if dataSet[j]>dataSet[j+1] {
-            
+            isSwapping = true
             // Swap values when left value is greater than the right value
             let temporaryValue = dataSet[j]     // Set aside the left value
             dataSet[j] = dataSet[j+1]           // Replace left value with right
             dataSet[j+1] = temporaryValue       // Replace right value with left
+            
         }
-
+        
+        
     }
-
+    
+    if isSwapping == false {
+        break
+    }
+    
+    
+    sorted += 1
+    
+    
+    
     // Print the array after 'n' passes
     print("Array after pass \(i+1)")
     print(dataSet)
     waitForInput()
-
+    
 }
 
 // Print the sorted array
